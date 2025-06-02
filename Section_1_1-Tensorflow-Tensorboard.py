3dcfd2306ee772c8624afca3d16a22132ff05e65
robbarto2 <robert_barton@yahoo.com>
2025-06-01 23:05:59 -0700

    Updates

--- Section_1_1-Tensorflow-Tensorboard.py
+++ Section_1_1-Tensorflow-Tensorboard.py
@@ -22,11 +22,10 @@ model = Sequential([
 model.compile(optimizer=Adam(learning_rate=0.01), loss='mse')
 
 # Create a TensorBoard callback
-<<<<<<< HEAD
+
 log_dir = "logs/"
-=======
 log_dir = "logs/logs"
->>>>>>> master
+
 tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 # Train the model
